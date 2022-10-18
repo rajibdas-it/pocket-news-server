67f2fca3d065000926e9072a0406044895370f08
rajibdas-it <rajibrad@gmail.com>
2022-10-18 14:21:07 +0600

    category link created

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+
+app.get("/", (req, res) => {
+  res.send("Pocket News Running");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.listen(port, () => {
+  console.log("server running on port", port);
+});
